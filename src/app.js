c456da3dbf3b71ab7bc2c4e7b8173a2acebfa703
Valter Cardoso <valterinfomundo@gmail.com>
2023-06-01 18:07:13 +0100

    Add welcome message.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ const taskRouter = require('./routers/task');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Welcome to the Task Manager API!</h1>');
+    res.end();
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
